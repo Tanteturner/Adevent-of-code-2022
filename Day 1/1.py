32f423ffa1b3acf7ab75d82b55bea057492d77f4
Jannis <jannis.matthies@gmail.com>
2022-12-05 21:03:20 +0100

    Day 1

---
+++ Day 1/1.py
@@ -0,0 +1,25 @@
+def readInput():
+    lines = []
+    with open(r"D:\Files\Workpaces\Adevent of code 2022\Day 1\input.txt", "r") as file1:
+        lines = file1.readlines()
+        file1.close()
+    return lines
+
+
+lines = readInput()
+
+elves = []
+currentElf = 0
+
+for i in range(len(lines)):
+    line = lines[i]
+    if line == "\n":
+        currentElf += 1
+    else:
+        if len(elves) <= currentElf:
+            elves.append(0)
+        elves[currentElf] += int(line)
+
+elves.sort(reverse=True)
+
+print(elves[0])
